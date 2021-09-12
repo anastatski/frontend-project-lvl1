ce1248a8374d4ac3f8751a3b72f42a48c7679bf4
Anastasiia Silanteva <misssmith@bk.ru>
2021-09-12 14:18:34 +0300

    4th step

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
-//@ts-check
+// @ts-check
 import readlineSync from 'readline-sync';
 
 const getUserName = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = readlineSync.question('May I have your name?');
-    console.log(`Hello, ${userName}!`);
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name?');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default getUserName;
